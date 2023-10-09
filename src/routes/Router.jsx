764df4c887a305c746a5ce0fcff29c04e3b669ae
sumaiyarimu22 <sumaiyarimu22@gmail.com>
2023-10-09 20:27:09 +0600

    update

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -4,13 +4,11 @@ import Home from "../components/Home";
 import Shop from "../components/Shop";
 import Cart from "../components/Carts";
 import About from "../components/About";
-import ErrorPage from "../components/ErrorPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Main />,
-    errorElement: <ErrorPage />,
 
     children: [
       {
